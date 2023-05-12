62e51a67d54ab6c176b5a936eb5a6ac2abfbf0f4
Terenceonye <onyeweketerence@gmail.com>
2023-05-12 07:50:13 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,49 @@
 #!/usr/bin/python3
-
-"""Script that reads stdin line by line and computes metrics"""
-
-import sys
-
-
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+"""
+Task: 0. Log Parsing
+File: 0x06-log_parsing/0-stats.py
+"""
+from sys import stdin
 
 
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
+def printstats(file_size, status_codes):
+    """
+    This prints statistics at the beginning and every 10 lines
+    This will also be called on a Keyboard interruption
+    """
+    print("File size: " + str(file_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(code + ": " + str(status_codes[code]))
 
-count = 0
-size = 0
+
+line_num = 0
+file_size = 0
+status_code = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 
 try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
+    for line in stdin:
+        line_num += 1
+        split_line = line.split()
 
-        stlist = line.split()
-        count += 1
+        if len(split_line) > 1:
+            file_size += int(split_line[-1])
 
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
+        if len(split_line) > 2 and split_line[-2].isnumeric():
+            status_code = split_line[-2]
+        else:
+            status_code = 0
 
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
+        if status_code in status_codes.keys():
+            status_codes[status_code] += 1
 
+        if line_num % 10 == 0:
+            printstats(file_size, status_codes)
 
-except KeyboardInterrupt:
-    printsts(sts, size)
+    printstats(file_size, status_codes)
+
+except (KeyboardInterrupt):
+    printstats(file_size, status_codes)
     raise
